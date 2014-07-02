0a9190b1631b095d9c790703949bc0b91a9c6311
jzimmerman95 <jessicazimmerman95@gmail.com>
2014-07-03 00:05:34 +0200

    added models, configured sign in

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-	has_and_belongs_to_many :users
-	belongs_to :admin, :classname => "User", :foreign_key => "admin_id"
+	has_and_belongs_to_many(:users, :join_table => "users_events")
+	belongs_to :admin, :class_name => "User", :foreign_key => "admin_id"
 end
