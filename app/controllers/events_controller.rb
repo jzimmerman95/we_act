63912478391f0583157f009167073be3040cab24
jzimmerman95 <jessicazimmerman95@gmail.com>
2014-07-04 00:41:40 +0200

    create event button added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,26 @@
 class EventsController < ApplicationController
 	before_action :authenticate_user!
 
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params.merge({:admin_id => current_user.id}))
+		if @event.save
+			redirect_to events_path
+		else
+			render :new
+		end
+	end
+
 	def index
 		@events = Event.all
+	end
+
+	private
+
+	def event_params
+		params.require(:event).permit(:title, :description, :city, :state, :address, :event_time)
 	end
 end
